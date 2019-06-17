d96e1d0e639073dbc72c158f198f6da9518e5b74
郑军 <zhengjun@laji.io>
2019-06-17 17:34:53 +0800

    获取手机的厂商信息

--- app/src/main/java/com/coindom/jpshdemo/JPushDemoApp.java
+++ app/src/main/java/com/coindom/jpshdemo/JPushDemoApp.java
@@ -2,6 +2,8 @@ package com.coindom.jpshdemo;
 
 import android.app.Application;
 import android.content.IntentFilter;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.content.LocalBroadcastManager;
 
 import cn.jpush.android.api.JPushInterface;
@@ -9,6 +11,7 @@ import cn.jpush.android.api.JPushInterface;
 public class JPushDemoApp extends Application {
 
     private JPushReceiver jPushReceiver;
+    private static final String TAG = "JPushDemoApp";
 
     @Override
     public void onCreate() {
@@ -23,5 +26,12 @@ public class JPushDemoApp extends Application {
         filter.addAction(JPushInterface.ACTION_REGISTRATION_ID);
         filter.addAction(JPushInterface.ACTION_NOTIFICATION_RECEIVED);
         LocalBroadcastManager.getInstance(this).registerReceiver(jPushReceiver, filter);
+
+//        读取设备硬件信息
+        MyUtils.println(TAG, "android.os.Build.VERSION.SDK = " + Build.VERSION.SDK);
+        MyUtils.println(TAG, "android.os.Build.MODEL = " + Build.MODEL);
+        MyUtils.println(TAG, "android.os.Build.VERSION.RELEASE = " + Build.VERSION.RELEASE);
+        MyUtils.println(TAG, "android.os.Build.MANUFACTURER = " + Build.MANUFACTURER);
+        MyUtils.println(TAG, "Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID) = " + Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID));
     }
 }
---
+++ app/src/main/java/com/coindom/jpshdemo/MyUtils.java
@@ -0,0 +1,7 @@
+package com.coindom.jpshdemo;
+
+class MyUtils {
+    public static void println(String tag, String content) {
+        System.out.println(tag +"  ****************  "+ content);
+    }
+}
